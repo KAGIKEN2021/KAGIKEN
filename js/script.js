9361585df2252eae7168b76e757cc48940eb8d6d
KAGIKEN2021 <96512657+KAGIKEN2021@users.noreply.github.com>
2022-02-07 00:53:42 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,397 @@
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 1000,//時間指定(1000＝1秒)
+	strokeWidth: 0.2,//進捗ゲージの太さ
+	color: '#555',//進捗ゲージのカラー
+	trailWidth: 0.2,//ゲージベースの線の太さ
+	trailColor: '#bbb',//ゲージベースの線のカラー
+	text: {//テキストの形状を直接指定				
+		style: {//天地中央に配置
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-30px 0 0 0',//バーより上に配置
+			transform: 'translate(-50%,-50%)',
+			'font-size': '1rem',
+			color: '#fff',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function (state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});
+
+// eachTextAnimeにappeartextというクラス名を付ける定義
+function EachTextAnimeControl() {
+	$('.eachTextAnime').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("appeartext");
+
+		} else {
+			$(this).removeClass("appeartext");
+		}
+	});
+}
+//spanタグを追加する
+var element = $(".eachTextAnime");
+element.each(function () {
+	var text = $(this).text();
+	var textbox = "";
+	text.split('').forEach(function (t, i) {
+		if (t !== " ") {
+			if (i < 10) {
+				textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+			} else {
+				var n = i / 10;
+				textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+			}
+
+		} else {
+			textbox += t;
+		}
+	});
+	$(this).html(textbox);
+});
+
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+
+	//=====ここからローディングエリア（splashエリア）を0.8秒でフェードアウトした後に動かしたいJSをまとめる
+	$("#splash").delay().fadeOut(800, function () {//#splashエリアをフェードアウトした後にアニメーションを実行
+
+		$('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+
+		EachTextAnimeControl()
+	});
+	//=====ここまでローディングエリア（splashエリア）を0.8秒でフェードアウトした後に動かしたいJSをまとめる
+
+});	
+
+//ドロップダウンの設定を関数でまとめる
+function mediaQueriesWin(){
+	var width = $(window).width();
+	if(width <= 768) {//横幅が768px以下の場合 $(".has-child>a").off('click');	//has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
+		$(".has-child>a").on('click', function() {//has-childクラスがついたaタグをクリックしたら
+			var parentElem =  $(this).parent();// aタグから見た親要素のliを取得し
+			$(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
+			$(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
+			return false;//リンクの無効化
+		});
+	}else{//横幅が768px以上の場合
+		$(".has-child>a").off('click');//has-childクラスがついたaタグのonイベントをoff(無効)にし
+		$(".has-child>a").removeClass('active');//activeクラスを削除
+		$('.has-child').children('ul').css("display","");//スライドトグルで動作したdisplayも無効化にする
+	}
+}
+
+// ページがリサイズされたら動かしたい場合の記述
+$(window).resize(function() {
+	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load',function(){
+	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
+
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200){//上から200pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+
+
+particlesJS("particles-js",{
+	"particles":{
+		"number":{
+			"value":38,//この数値を変更すると幾何学模様の数が増減できる
+			"density":{
+				"enable":true,
+				"value_area":800
+			}
+		},
+		"color":{
+			"value":"#ffffff"//色
+		},
+		"shape":{
+			"type":"polygon",//形状はpolygonを指定
+			"stroke":{
+				"width":0,
+			},
+	"polygon":{
+		"nb_sides":3//多角形の角の数
+	},
+	"image":{
+		"width":190,
+		"height":100
+	}
+	},
+		"opacity":{
+		"value":0.664994832269074,
+		"random":false,
+		"anim":{
+			"enable":true,
+			"speed":2.2722661797524872,
+			"opacity_min":0.08115236356258881,
+			"sync":false
+		}
+		},
+		"size":{
+			"value":3,
+			"random":true,
+			"anim":{
+				"enable":false,
+				"speed":40,
+				"size_min":0.1,
+				"sync":false
+			}
+		},
+		"line_linked":{
+			"enable":true,
+			"distance":150,
+			"color":"#ffffff",
+			"opacity":0.6,
+			"width":1
+		},
+		"move":{
+			"enable":true,
+			"speed":6,//この数値を小さくするとゆっくりな動きになる
+			"direction":"none",//方向指定なし
+			"random":false,//動きはランダムにしない
+			"straight":false,//動きをとどめない
+			"out_mode":"out",//画面の外に出るように描写
+			"bounce":false,//跳ね返りなし
+			"attract":{
+				"enable":false,
+				"rotateX":600,
+				"rotateY":961.4383117143238
+			}
+		}
+	},
+	"interactivity":{
+		"detect_on":"canvas",
+		"events":{
+			"onhover":{
+				"enable":false,
+				"mode":"repulse"
+			},
+	"onclick":{
+		"enable":false
+	},
+	"resize":true
+		}
+	},
+	"retina_detect":true
+});
+
+$('a[href^="#"]').click(function () {
+	const speed = 600;
+	let href = $(this).attr("href");
+	let target = $(href == "#" || href == "" ? "html" : href);
+	let position = target.offset().top;
+	$("body,html").animate({ scrollTop: position }, speed, "swing");
+	return false;
+  });
+
+
+// smoothTriggerにsmoothTextAppearというクラス名を付ける定義
+function SmoothTextAnime() {
+	$('.smoothTextTrigger').each(function(){ //smoothTextTriggerというクラス名が
+	  var elemPos = $(this).offset().top-50;//要素より、50px上の
+	  var scroll = $(window).scrollTop();
+	  var windowHeight = $(window).height();
+	  if (scroll >= elemPos - windowHeight){
+	  $(this).addClass('smoothTextAppear');// 画面内に入ったらsmoothTextAppearというクラス名を追記
+	  }else{
+	  $(this).removeClass('smoothTextAppear');// 画面外に出たらsmoothTextAppearというクラス名を外す
+	  }
+	  }); 
+  }
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+	SmoothTextAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function () {
+	SmoothTextAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+	$('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+		}else{
+		$(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+		}
+		});
+
+		$('.zoomInTrigger').each(function(){ //zoomInTriggerというクラス名が
+			var elemPos = $(this).offset().top-50;//要素より、50px上の
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll >= elemPos - windowHeight){
+			$(this).addClass('zoomIn');// 画面内に入ったらzoomInというクラス名を追記
+			}else{
+			$(this).removeClass('zoomIn');// 画面外に出たらzoomInというクラス名を外す
+			}
+			});
+		
+			$('.fadeInTrigger').each(function(){ //fadeInTriggerというクラス名が
+				var elemPos = $(this).offset().top-10;//要素より、50px上の
+				var scroll = $(window).scrollTop();
+				var windowHeight = $(window).height();
+				if (scroll >= elemPos - windowHeight){
+				$(this).addClass('fadeIn');// 画面内に入ったらfadeInというクラス名を追記
+				}else{
+				$(this).removeClass('fadeIn');// 画面外に出たらfadeInというクラス名を外す
+				}
+				});
+
+				$('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+					var elemPos = $(this).offset().top-50;//要素より、50px上の
+					var scroll = $(window).scrollTop();
+					var windowHeight = $(window).height();
+					if (scroll >= elemPos - windowHeight){
+					$(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+					}else{
+					$(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+					}
+					});
+  }
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+	  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面をスクロールをしたら動かしたい場合の記述
+  
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+	$(window).on('load', function(){
+	  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+// TextTypingというクラス名がついている子要素（span）を表示から非表示にする定義
+function TextTypingAnime() {
+	$('.TextTyping').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		var thisChild = "";
+		if (scroll >= elemPos - windowHeight) {
+			thisChild = $(this).children(); //spanタグを取得
+			//spanタグの要素の１つ１つ処理を追加
+			thisChild.each(function (i) {
+				var time = 100;
+				//時差で表示する為にdelayを指定しその時間後にfadeInで表示させる
+				$(this).delay(time * i).fadeIn(time);
+			});
+		} else {
+			thisChild = $(this).children();
+			thisChild.each(function () {
+				$(this).stop(); //delay処理を止める
+				$(this).css("display", "none"); //spanタグ非表示
+			});
+		}
+	});
+}
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	//spanタグを追加する
+	var element = $(".TextTyping");
+	element.each(function () {
+		var text = $(this).html();
+		var textbox = "";
+		text.split('').forEach(function (t) {
+			if (t !== " ") {
+				textbox += '<span>' + t + '</span>';
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+
+	});
+
+	TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+/*===========================================================*/
+/*機能編 9-2-2	任意の場所をクリックすると隠れていた内容が開き、先に開いていた内容が閉じる*/
+/*===========================================================*/
+//アコーディオンをクリックした時の動作
+$('.title1').on('click', function() {//タイトル要素をクリックしたら
+	$('.box1').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
+    
+	var findElm = $(this).next(".box1");//タイトル直後のアコーディオンを行うエリアを取得
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去    
+	}else{//それ以外は
+		$('.close').removeClass('close'); //クラス名closeを全て除去した後
+		$(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
+		$(findElm).slideDown(500);//アコーディオンを開く
+	}
+});
+
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});
+
